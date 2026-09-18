683cf575cca3c16d87e9ad445269cf932e1a3d60
A112222016 <a112222016@mail.shu.edu.tw>
2026-09-18 16:00:20 +0800

    第一週作業 我還在努力

--- 115_09_18/Java-01/src/HelloCourse.java
+++ 115_09_18/Java-01/src/HelloCourse.java
@@ -1,10 +1,9 @@
 package src;
 
 public class HelloCourse{
-    Run|Debug
     public static void main(String[] args) {
         // 固定輸出，方便比較不同工具的執行結果
-        System.out.println(x: "Hello, Java!");
-        System.out.println(x: "Course: Object-Oriented Programming");
+        System.out.println("Hello, Java!");
+        System.out.println("Course: Object-Oriented Programming");
     }
 }
